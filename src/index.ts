67f06a0c79ad85c248445b0948b661286f6a685b
[Kene Nwogu] <nwogujoseph1111@gmail.com>
2023-04-08 00:31:20 +0100

    use environment port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import app from "./app"
 import { prisma } from "./db/db.client"
 
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, async () => {
+app.listen(port, async () => {
     await prisma.$connect()
-    console.log('Server listening on http://localhost:3000')
+    console.log(`Server listening on http://localhost:${port}`)
 })
 
 
